f245f6ea496629e8141337146b2b17a1a32c3011
sullis <github@seansullivan.com>
2024-05-31 16:34:52 -0700

    epoll is available on linux

--- src/test/java/io/github/sullis/servicetalk/playground/HelloWorldTest.java
+++ src/test/java/io/github/sullis/servicetalk/playground/HelloWorldTest.java
@@ -1,9 +1,20 @@
 package io.github.sullis.servicetalk.playground;
 
+import io.netty.channel.epoll.Epoll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class HelloWorldTest {
+  @Test
+  @EnabledOnOs(value = OS.LINUX)
+  void epollIsAvailableOnLinux() throws Throwable {
+    assertThat(Epoll.isAvailable()).isTrue();
+  }
+
   @Test
   void hello() {
     // TODO
