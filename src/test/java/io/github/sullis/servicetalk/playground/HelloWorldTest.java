c03d150d3ab43fdacca8642a4e14b21a5140b90f
sullis <github@seansullivan.com>
2024-05-31 17:00:02 -0700

    use Netty leak detector extension (#4)

--- src/test/java/io/github/sullis/servicetalk/playground/HelloWorldTest.java
+++ src/test/java/io/github/sullis/servicetalk/playground/HelloWorldTest.java
@@ -1,5 +1,6 @@
 package io.github.sullis.servicetalk.playground;
 
+import io.github.nettyplus.leakdetector.junit.NettyLeakDetectorExtension;
 import io.netty.channel.epoll.Epoll;
 import io.servicetalk.http.api.HttpClient;
 import io.servicetalk.http.api.HttpServerContext;
@@ -8,12 +9,14 @@ import io.servicetalk.http.netty.HttpServers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static io.servicetalk.concurrent.api.Single.succeeded;
 import static io.servicetalk.http.api.HttpSerializers.textSerializerUtf8;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+@ExtendWith(NettyLeakDetectorExtension.class)
 public class HelloWorldTest {
   @Test
   @EnabledOnOs(value = OS.LINUX)
